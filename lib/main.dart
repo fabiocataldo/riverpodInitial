c3f782732c4379b6695974367fd82d1edf2f57e5
Fabio Cataldo <f.cataldo@rdisolutions.co.uk>
2022-12-08 14:39:20 +0000

    increment counter and datetime

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,16 +20,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+class Counter extends StateNotifier<int?> {
+  // Counter(super.state);
+  Counter() : super(null);
+  void increment() => state = state == null ? 1 : state + 1;
+  int? get value => state;
+}
+
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter(),
+);
+
+class GetDate extends StateNotifier<DateTime> {
+  GetDate(super.state);
+
+  void dateTime() => state = DateTime.now();
+
+  DateTime? get value => state;
+}
+
+final currentDate = StateNotifierProvider<GetDate, DateTime>(
+  (ref) => GetDate(DateTime.now()),
+);
+
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomePage'),
+      appBar: AppBar(title: Consumer(builder: ((context, ref, child) {
+        final count = ref.watch(counterProvider);
+        final text = count ?? 'Press the Button';
+        return Text(text.toString());
+      }))),
+      body: Center(
+        child: Column(children: [
+          TextButton(
+            onPressed: () {
+              ref.read(counterProvider.notifier).increment();
+              ref.read(currentDate.notifier).dateTime();
+            },
+            child: const Text('Increment Counter'),
+          ),
+          Consumer(builder: ((context, ref, child) {
+            final date = ref.watch(currentDate);
+            return Text(date.toIso8601String());
+          }))
+        ]),
       ),
-      body: const Center(),
     );
   }
 }
